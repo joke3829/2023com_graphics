d3b9f08c037238f389eaf50c8dfbb3f3b3b0b153
joke3829 <kim1234312@gmail.com>
2023-10-13 23:59:33 +0900

    Create main.cpp

---
+++ 피라미드 띄우기/main.cpp
@@ -0,0 +1,94 @@
+#include "Filetobuf.h"
+#include "pyramid.h"
+
+void make_vertexShaders();		// vertexShader 생성 함수
+void make_fragmentShaders();	// fragmentShader 생성함수
+GLuint make_shaderProgram();	// 최종 셰이더 프로그램 생성함수
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+int width, height;
+GLuint shaderProgramID;			// 셰이더 프로그램 이름
+GLuint vertexShader;			// vertexShader 객체
+GLuint fragmentShader;			// fragment 객체
+
+Mesh pyramid;
+
+void main(int argc, char** argv)
+{
+	width = height = 800;
+	//윈도우 생성하기
+	glutInit(&argc, argv);							// glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);	// 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100);				// 윈도우의 위치 지정
+	glutInitWindowSize(width, height);					// 윈도우의 크기 지정
+	glutCreateWindow("VAO_VBO");					// 윈도우 생성(윈도우 이름)
+
+	//GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)						//glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	shaderProgramID = make_shaderProgram();			// 셰이더 프로그램 만들기
+	pyramid.Initialize(&shaderProgramID);
+	glutDisplayFunc(drawScene);						// 출력 함수의 지정
+	glutReshapeFunc(Reshape);						// 다시 그리기 함수 지정
+	glutMainLoop();									// 이벤트 처리 시작
+}
+
+GLvoid drawScene()									// 콜백 함수: 그리기 콜백 함수
+{
+	glUseProgram(shaderProgramID);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	pyramid.Draw();
+	glutSwapBuffers();								// 화면에 출력하기
+}
+
+GLvoid Reshape(int w, int h)						// 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+	vertexSource = filetobuf("VertexShader.glsl");
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+}
+
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+	fragmentSource = filetobuf("FragmentShader.glsl");
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+}
+
+GLuint make_shaderProgram()
+{
+	make_vertexShaders();							// vertexShader 만들기
+	make_fragmentShaders();							// 프래그먼트 셰이더 만들기
+	GLuint shaderID = glCreateProgram();
+
+	glAttachShader(shaderID, vertexShader);
+	glAttachShader(shaderID, fragmentShader);
+
+	glLinkProgram(shaderID);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glUseProgram(shaderID);
+	return shaderID;
+}

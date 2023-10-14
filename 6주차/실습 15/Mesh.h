08a30563abc79b508f317eed6e281937e80006ad
joke3829 <kim1234312@gmail.com>
2023-10-14 16:14:05 +0900

    Create Mesh.h

---
+++ 6주차/실습 15/Mesh.h
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+class Mesh {
+private:
+	int vari;
+	bool view_polygon;
+	unsigned int triangle_num;
+	GLuint VAO, VBO[2], EBO;
+	std::vector<glm::vec3> vertexs;
+	std::vector<glm::vec3> colors;
+	std::vector<unsigned int> index;
+
+	glm::mat4 FinalTransformMatrix;
+	glm::mat4 transMatrix;
+	glm::mat4 rotateMatrix;
+	glm::mat4 scaleMatrix;
+	bool ReadOBJ(std::string);		//OBJ읽어오기
+	GLuint* shader;
+public:
+	Mesh();
+	~Mesh();
+	void Initialize(GLuint*, std::string);				// vao, vbo 등을 생성 및 초기화	생성자에 넣을것
+	void Draw();			//그리기 함수
+	void Rotate(float, float, float ,float);			// 회전
+	void Scale(float);									// 신축
+	void Move(float, float, float);						// 이동
+	void TransView(bool);
+	void trans_Initialize();
+};

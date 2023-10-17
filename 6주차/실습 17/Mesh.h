39cae6abd1babd1d178e0ca0938011502b68e16f
joke3829 <kim1234312@gmail.com>
2023-10-17 18:53:13 +0900

    Update Mesh.h

--- 6주차/실습 17/Mesh.h
+++ 6주차/실습 17/Mesh.h
@@ -14,10 +14,16 @@ private:
 	glm::mat4 scaleMatrix;
 	glm::mat4 rotate_world;			//공전용
 	glm::mat4 finalrotate;			//자리 잡기용
+	glm::mat4 scale_world;
 	bool ReadOBJ(std::string);		//OBJ읽어오기
 	GLuint* shader;
 public:
-	float move_dis;
+	float move_dis_x;
+	float move_dis_y;
+	float move_dis_z;
+	float scale_size;
+	float scale_world_size;
+	float rotate_wolrd_rad;
 	Mesh();
 	~Mesh();
 	void Initialize(GLuint*, std::string);				// vao, vbo 등을 생성 및 초기화	생성자에 넣을것
@@ -26,6 +32,7 @@ public:
 	void Scale(float);									// 신축
 	void Move(float, float, float);						// 이동
 	void Rotate_world(float, float, float, float);
+	void Scale_world(float);
 	void T_format();
 	void M_format();
 };
